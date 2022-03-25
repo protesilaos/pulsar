6258c511ee4c27cc1f794224d16f95f396765ec8
Protesilaos Stavrou <info@protesilaos.com>
2022-03-25 12:09:34 +0200

    Abstract pulsar-mode lighter
    Thanks to Rudolf Adamkovič for the feedback in issue:
    <https://gitlab.com/protesilaos/pulsar/-/issues/11>.

--- pulsar.el
+++ pulsar.el
@@ -295,11 +295,14 @@ default)."
 
 ;;;; Mode setup
 
+(defvar pulsar-lighter " -P-"
+  "Mode line lighter used by `pulsar-mode'.")
+
 (define-minor-mode pulsar-mode
   "Set up pulsar for each function in `pulsar-pulse-functions'.
 This is a buffer-local mode.  Also check `pulsar-global-mode'."
   :global nil
-  :lighter " -P-"
+  :lighter pulsar-lighter
   (if pulsar-mode
       (add-hook 'post-command-hook #'pulsar--post-command-pulse nil 'local)
     (remove-hook 'post-command-hook #'pulsar--post-command-pulse 'local)))
