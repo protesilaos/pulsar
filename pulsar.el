b8e6468f4e0e002c2adf57bd042e411ef0a006dc
Protesilaos Stavrou <info@protesilaos.com>
2022-04-02 06:46:07 +0300

    Refine how pulsar-highlight-dwim uses the region
    This way we do not get an error about the empty mark-ring when entering
    a new buffer and calling this command.

--- pulsar.el
+++ pulsar.el
@@ -332,18 +332,17 @@ default)."
   (add-hook 'deactivate-mark-hook #'pulsar--remove-face-remap nil t))
 
 ;;;###autoload
-(defun pulsar-highlight-dwim (beg end)
+(defun pulsar-highlight-dwim ()
   "Temporarily highlight the current line or active region.
-The region is the space between the BEG and END positions.  The
-region may also be a rectangle.
+The region may also be a rectangle.
 
 For lines, do the same as `pulsar-highlight-line'."
-  (interactive "r")
+  (interactive)
   (cond
    ((bound-and-true-p rectangle-mark-mode)
     (pulsar--highlight-rectangle))
    ((use-region-p)
-    (pulsar--pulse :no-pulse pulsar-highlight-face beg end))
+    (pulsar--pulse :no-pulse pulsar-highlight-face (region-beginning) (region-end)))
    (t
     (pulsar--pulse :no-pulse pulsar-highlight-face))))
 
