cdb7892a2b7e8e7cc644b8e24e5ab6bf0c51584d
Protesilaos Stavrou <info@protesilaos.com>
2022-04-08 10:19:44 +0300

    Upgrade to version 0.3.0

--- pulsar.el
+++ pulsar.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://git.sr.ht/~protesilaos/pulsar
-;; Version: 0.2.4
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, pulse, highlight
 
