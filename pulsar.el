b7915dd060818fed16578480f4181a0d566d487c
Protesilaos Stavrou <info@protesilaos.com>
2022-08-19 09:52:18 +0300

    Read real-this-command if necessary
    The need for this arrangement became clear after trying to make the
    scroll motions of the 'evil' package work with pulsar-pulse-functions.
    
    Thanks to Duy Nguyen for reporting the issue on the mailing list.  I
    helped find the source of the apparent problem in the 'evil' code base
    and then Tom Dalziel explained why 'evil' does things this way,
    suggesting 'real-this-command' as a possibility:
    
    - <https://lists.sr.ht/~protesilaos/pulsar/%3C89566F5C-25AD-4281-94CB-031FE8878119%40gmail.com%3E>
    - <https://lists.sr.ht/~protesilaos/pulsar/%3C87pmgy3vzq.fsf%40protesilaos.com%3E>
    - <https://github.com/emacs-evil/evil/issues/1659>

--- pulsar.el
+++ pulsar.el
@@ -393,7 +393,8 @@ This is a buffer-local mode.  Also check `pulsar-global-mode'."
 (defun pulsar--post-command-pulse ()
   "Run `pulsar-pulse-line' for `pulsar-pulse-functions'."
   (when (and (or pulsar-mode pulsar-global-mode)
-             (memq this-command pulsar-pulse-functions))
+             (or (memq this-command pulsar-pulse-functions)
+                 (memq real-this-command pulsar-pulse-functions)))
     (pulsar-pulse-line)))
 
 (make-obsolete 'pulsar-setup nil "0.3.0")
