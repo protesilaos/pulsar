880dc5e48d14c02a18d8bd00645af9779e036be9
ukiran03 <ushakiranreddi@gmail.com>
2024-10-06 10:47:09 +0530

    Minor enhancement to pulsar-pulse-region
    Now, it should pulse the region at the mark (if any).

--- pulsar.el
+++ pulsar.el
@@ -361,9 +361,9 @@ pulse effect."
 (defun pulsar-pulse-region ()
   "Temporarily highlight the active region if any."
   (interactive)
-  (when (region-active-p)
-    (let ((beg (region-beginning))
-          (end (region-end)))
+  (if (region-active-p)
+      (let ((beg (region-beginning))
+            (end (region-end)))
       ;; FIXME 2024-08-29: Finding the lines and columns therein
       ;; does not work because consecutive pulses cancel each
       ;; other out, leaving only the last one active.
@@ -381,7 +381,11 @@ pulse effect."
       ;;     (setq beg (progn (move-to-column begcol) (point))
       ;;           end (progn (move-to-column endcol) (point))))
       ;;   (pulsar--pulse nil nil beg end)))
-      (pulsar--pulse nil nil beg end))))
+        (pulsar--pulse nil nil beg end))
+    (when (mark)
+      (let ((beg (mark))
+            (end (point)))
+        (pulsar--pulse nil nil beg end)))))
 
 ;;;###autoload
 (defun pulsar-highlight-line ()
